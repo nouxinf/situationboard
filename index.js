944c22bac822111835894048e61b2211d11ed30f
nouxinf <180769756+nouxinf@users.noreply.github.com>
2026-06-12 16:06:27 +0100

    some html

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const port = 3055;
 app.use(express.static("public"));
 
 app.listen(port, () => {
-  console.log(`Situationboard listening on port ${port}`);
-  console.log(`Open it up at http://localhost:${port}`);
+	console.log(`Situationboard listening on port ${port}`);
+	console.log(`Open it up at http://localhost:${port}`);
 });
