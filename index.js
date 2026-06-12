bf8e0aec5e10fab90a3a228b2e9e5bf6898b4e39
nouxinf <180769756+nouxinf@users.noreply.github.com>
2026-06-12 15:36:49 +0100

    expressjs boilerplate

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 3055;
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Situationboard listening on port ${port}`);
+  console.log(`Open it up at http://localhost:${port}`);
+});
