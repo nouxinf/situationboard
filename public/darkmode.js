2348fb40e9c61848afc46b50905f4bab0b99790f
nouxinf <180769756+nouxinf@users.noreply.github.com>
2026-09-18 18:36:20 +0100

    add proper toggle

--- public/darkmode.js
+++ public/darkmode.js
@@ -1,5 +1,27 @@
 const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
-if (localStorage.getItem("theme") === "dark" || systemSettingDark.matches) {
+if (localStorage.getItem("theme") === "dark") {
 	document.documentElement.setAttribute("data-theme", "dark");
 	localStorage.setItem("theme", "dark");
+} else if (localStorage.getItem("theme") === "light") {
+	document.documentElement.setAttribute("data-theme", "light");
+	localStorage.setItem("theme", "light");
+} else {
+	if (systemSettingDark.matches) {
+		document.documentElement.setAttribute("data-theme", "dark");
+		localStorage.setItem("theme", "dark");
+	} else {
+		localStorage.setItem("theme", "light");
+	}
 }
+
+const themeToggle = document.getElementById("theme-toggle");
+themeToggle.addEventListener("click", () => {
+	const currentTheme = document.documentElement.getAttribute("data-theme");
+	if (currentTheme === "dark") {
+		document.documentElement.setAttribute("data-theme", "light");
+		localStorage.setItem("theme", "light");
+	} else {
+		document.documentElement.setAttribute("data-theme", "dark");
+		localStorage.setItem("theme", "dark");
+	}
+});
